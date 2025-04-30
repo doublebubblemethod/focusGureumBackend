09a087ed650dd6dbfacbe5f0f727da5f60b63034
doublebubblemethod <antihobbit@protonmail.com>
2025-04-30 19:18:44 +0200

    initial commit

---
+++ src/main/java/com/focusGureumWebApp/focusGureumWebdemo/FocusGureumWebdemoApplication.java
@@ -0,0 +1,13 @@
+package com.focusGureumWebApp.focusGureumWebdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusGureumWebdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusGureumWebdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/focusGureumWebApp/focusGureumWebdemo/FocusGureumWebdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.focusGureumWebApp.focusGureumWebdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusGureumWebdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
